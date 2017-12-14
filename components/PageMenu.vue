4366c6a499a75c24fa2e839699ebbdc1d0be67ea
Horace Keung <horacekeung@hotmail.com>
2017-12-14 23:52:06 +0000

    better menu

--- components/PageMenu.vue
+++ components/PageMenu.vue
@@ -1,11 +1,11 @@
 <template>
 	<div class="container fixed-top">
-		<div class="logo-overlay">
-			<img width="100" src="~/assets/img/logo.png">
+		<div class="logo-overlay bg-white60">
+			<img src="~/assets/img/logo.png">
 		</div>
-		<div class="float-right pt-5">
-			<ul>
-				<li class="d-inline text-white ml-3 pointer" v-for="l in links" @click="scrollTo(l.eleId)">{{lang[l.name]}}</li>
+		<div class="float-right">
+			<ul :class="'menu-trans pl-0 pb-3 '+(isMenuWhite?'pt-5':'pt-3 bg-white60')">
+				<li :class="'menu-trans d-inline mx-3 pointer'+(isMenuWhite?' text-white':'')" v-for="l in links" @click="scrollTo(l.eleId)">{{lang[l.name]}}</li>
 			</ul>
 		</div>
 	</div>
@@ -14,6 +14,12 @@
 <script>
 import util from '~/assets/js/util'
 export default {
+	computed: {
+		isMenuWhite () {
+			const height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight
+			return height * 0.9 > this.$store.state.scrollTop
+		}
+	},
 	data () {
 		return {
 			links: [
@@ -29,10 +35,24 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+@import '../assets/css/mixin.scss';
 .logo-overlay {
 	padding: 3rem 1rem 1rem;
-	background-color: rgba(255, 255, 255, 0.5);
 	position: absolute;
+}
+.logo-overlay img {
+	width: 100px;
+}
+.menu-trans {
+	transition: all 1s ease-out;
+}
+@include media-breakpoint-down(md) {
+	.logo-overlay {
+		padding: 1.5rem 0.5rem 0.5rem;
+	}
+	.logo-overlay img {
+		width: 70px;
+	}
 }
 </style>
